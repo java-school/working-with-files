125ade736eb638db0a33d224f5c3ddc5f63033c6
Igor Paprzycki <ipaprzycki@pzu.pl>
2018-09-10 15:58:26 +0200

    3. Deleting Files - Deleting non-empty directory throws Exception.

--- src/test/java/pl/javaschool/working_with_files/DeletingFilesTest.java
+++ src/test/java/pl/javaschool/working_with_files/DeletingFilesTest.java
@@ -3,6 +3,7 @@ package pl.javaschool.working_with_files;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.nio.file.DirectoryNotEmptyException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -42,5 +43,19 @@ public class DeletingFilesTest {
         assertFalse(Files.exists(path));
         //when
         Files.deleteIfExists(path);
+    }
+
+    @Test(expected = DirectoryNotEmptyException.class)
+    public void givenExistingNonEmptyDirectory_whenDelete_shouldThrowException() throws IOException {
+        //given
+        Path dirPath = Files.createTempDirectory(Paths.get(BASE_DIR), null);
+        assertTrue(Files.exists(dirPath));
+
+        Path filePath = dirPath.resolve("file.txt");
+        Files.createFile(filePath);
+        //when
+        Files.delete(dirPath); //this throws Exception
+        //then
+//      How to check if directory still exists?
     }
 }
